f78945eab524663cf2945ba6a3f73c710f91530b
strife.jave <j.strf0124@gmail.com>
2020-05-27 11:54:31 +0000

    add column and error method

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,6 +8,7 @@
         <table class="table table-striped">
             <thead>
                 <tr>
+                    <th>ステータス</th>
                     <th>タスク</th>
                     <th>登録日時</th>
                     <th>更新日時</th>
@@ -16,6 +17,7 @@
             <tbody>
                 @foreach ( $tasks as $task )
                 <tr>
+                    <td>{{ $task->status }}</td>
                     <td>{!! link_to_route('tasks.show', $task->content, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->created_at }}</td>
                     <td>{{ $task->updated_at }}</td>
